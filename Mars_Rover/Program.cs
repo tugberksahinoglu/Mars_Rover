62ca8f952d46d46844242077cef9998cf88b1103
Tuğberk Şahinoğlu <tugberk.sahinoglu@argenet.com.tr>
2022-02-09 19:28:40 +0300

    first commit

---
+++ Mars_Rover/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+
+namespace Mars_Rover {
+    class Program {
+        static void Main(string[] args) {
+            string[] fields = SetField();
+            string[] firstRoverLocation = SetRoverLocation(fields);
+            string firstRoverMoves = SetRoverMoves();
+            string[] secondRoverLocation = SetRoverLocation(fields);
+            string secondRoverMoves = SetRoverMoves();
+
+            var firstRover = new Rover(Convert.ToInt32(firstRoverLocation[0]), Convert.ToInt32(firstRoverLocation[1]), (Direction)Enum.Parse(typeof(Direction), firstRoverLocation[2]));
+            var secondRover = new Rover(Convert.ToInt32(secondRoverLocation[0]), Convert.ToInt32(secondRoverLocation[1]), (Direction)Enum.Parse(typeof(Direction), secondRoverLocation[2]));
+
+            firstRover.MoveInstructions(firstRoverMoves, fields);
+            Console.WriteLine(firstRover.GetFullPosition());
+
+            secondRover.MoveInstructions(secondRoverMoves, fields);
+            Console.WriteLine(secondRover.GetFullPosition());
+        }
+
+        private static string SetRoverMoves() {
+            string roverMoves;
+            bool isValidMove;
+            do {
+                Console.WriteLine("Write rover moves like LMLMLMLMM");
+                roverMoves = Console.ReadLine();
+                isValidMove = IsValidMove(roverMoves);
+                if (!isValidMove) {
+                    Console.WriteLine("Choose valid moves L(Left), R(Right), M(Move)");
+                }
+            } while (!isValidMove);
+            return roverMoves;
+        }
+
+        private static string[] SetRoverLocation(string[] field) {
+            string[] roverLocation;
+            bool isValidPosition;
+            do {
+                Console.WriteLine("Write rover location like 1 2 N");
+                roverLocation = Console.ReadLine().Split(' ');
+                isValidPosition = IsValidPosition(field, roverLocation);
+                if (!isValidPosition) {
+                    Console.WriteLine("Choose the location from within the field");
+                }
+            } while (!isValidPosition);
+            return roverLocation;
+        }
+
+        private static string[] SetField() {
+            string[] field;
+            bool isValidField;
+            do {
+                Console.WriteLine("Write field like 5 5");
+                field = Console.ReadLine().Split(' ');
+                isValidField = IsValidField(field);
+                if (!isValidField) {
+                    Console.WriteLine("Choose valid field");
+                }
+            } while (!isValidField);
+            return field;
+        }
+
+        private static bool IsValidPosition(string[] field, string[] roverLocation) {
+            if (roverLocation.Length != 3) {
+                return false;
+            }
+
+            if (!int.TryParse(roverLocation[0], out int positionX) || !int.TryParse(roverLocation[1], out int positionY)) {
+                return false;
+            }
+
+            if (roverLocation[2] != "N" && roverLocation[2] != "E" && roverLocation[2] != "S" && roverLocation[2] != "W") {
+                return false;
+            }
+
+            if (positionX < 0 || positionX > Convert.ToInt32(field[0])) {
+                return false;
+            }
+            if (positionY < 0 || positionY > Convert.ToInt32(field[1])) {
+                return false;
+            }
+
+            return true;
+        }
+
+        private static bool IsValidMove(string moves) {
+            foreach (var move in moves) {
+                if (!(move.Equals('L') || move.Equals('R') || move.Equals('M'))) {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private static bool IsValidField(string[] field) {
+            if (field.Length != 2) {
+                return false;
+            }
+
+            if (!int.TryParse(field[0], out int firstField) || !int.TryParse(field[1], out int secondField)) {
+                return false;
+            }
+
+            if (firstField <= 0 || secondField <= 0) {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ Mars_Rover/Rover.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace Mars_Rover {
+    internal class Rover {
+        private int positionX { get; set; }
+
+        private int positionY { get; set; }
+
+        private Direction direction { get; set; }
+
+        public Rover(int positionX, int positionY, Direction direction) {
+            this.positionX = positionX;
+            this.positionY = positionY;
+            this.direction = direction;
+        }
+
+        private void Move(string[] fields) {
+            switch (direction) {
+                case Direction.N:
+                    if (positionY + 1 > Convert.ToInt32(fields[0])) {
+                        throw new Exception("Out of bounds");
+                    }
+                    positionY++;
+                    break;
+                case Direction.E:
+                    if (positionX + 1 > Convert.ToInt32(fields[1])) {
+                        throw new Exception("Out of bounds");
+                    }
+                    positionX++;
+                    break;
+                case Direction.S:
+                    if (positionY - 1 < 0) {
+                        throw new Exception("Out of bounds");
+                    }
+                    positionY--;
+                    break;
+                case Direction.W:
+                    if (positionX - 1 < 0) {
+                        throw new Exception("Out of bounds");
+                    }
+                    positionX--;
+                    break;
+            }
+        }
+
+        private void Turn(char side) {
+            if (side.Equals('R')) {
+                switch (direction) {
+                    case Direction.N:
+                        direction = Direction.E;
+                        break;
+                    case Direction.E:
+                        direction = Direction.S;
+                        break;
+                    case Direction.S:
+                        direction = Direction.W;
+                        break;
+                    case Direction.W:
+                        direction = Direction.N;
+                        break;
+                }
+            }
+            else if (side.Equals('L')) {
+                switch (direction) {
+                    case Direction.N:
+                        direction = Direction.W;
+                        break;
+                    case Direction.E:
+                        direction = Direction.N;
+                        break;
+                    case Direction.S:
+                        direction = Direction.E;
+                        break;
+                    case Direction.W:
+                        direction = Direction.S;
+                        break;
+                }
+            }
+        }
+
+        public void MoveInstructions(string instructions, string[] fields) {
+            foreach (var instruction in instructions) {
+                switch (instruction) {
+                    case 'M':
+                        Move(fields);
+                        break;
+                    case 'L':
+                    case 'R':
+                        Turn(instruction);
+                        break;
+                }
+            }
+        }
+
+        public string GetFullPosition() {
+            return $"{positionX} {positionY} {direction}";
+        }
+    }
+
+    public enum Direction {
+        [Description("North")]
+        N,
+        [Description("East")]
+        E,
+        [Description("South")]
+        S,
+        [Description("West")]
+        W
+    }
+}
